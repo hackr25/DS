6d7bc102350884d979da3a163e744366a71cb3fe
hackr25 <83638637+hackr25@users.noreply.github.com>
2023-10-13 06:26:27 +0530

    Add files via upload

--- selectionsort.py
+++ selectionsort.py
@@ -2,18 +2,16 @@
 
 def selection_sort(arr):
     n = len(arr)
-    for i in range(n):
-        # Find the index of the minimum element    
+    for i in range(n):  
         min_index = i
         for j in range(i+1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
-        # Swap the found minimum element with the first element
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
-    unsorted_array = [64, 34, 25, 12, 22, 11, 90]
-    sorted_array = selection_sort(unsorted_array)
-    print("Sorted array:", sorted_array)
+unsorted_array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = selection_sort(unsorted_array)
+print("Sorted array:", sorted_array)
 
 
